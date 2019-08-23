4e3342b1c3a730790e3a2c639d0f5f89aab29a0e
Rachele Suy <rachelesuy@gmail.com>
2019-08-23 14:58:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,5 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do 
     :index
+    user = User.create(:name => "Trisha", :email => "trisha@test.com", :fav_icecream => "mint chocolate chip")
   end
 end
